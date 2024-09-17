733e7b465b88022367bb6590318dc7f1cd77d56c
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-17 16:16:29 +0300

    salom

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './index.css'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='App'>
+
+      </div>
+    </>
+  )
+}
+
+export default App
