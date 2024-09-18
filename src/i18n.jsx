92034b9a68f06f60410dca5ea4733654fbb89986
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-18 13:20:40 +0300

    salom

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,25 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import uzTranslation from "../public/uzbek/uzbek.json"
-import ruTranslation from "../public/russian/russian.json"
-import enTranslation from "../public/english/english.json"
+import uzTranslation from '../public/uzbek/uzbek.json'; // To'g'ri fayl
+import ruTranslation from '../public/russian/russian.json';
+import enTranslation from '../public/english/english.json';
+
 i18n
-    .use(Backend)
-
-    .use(LanguageDetector)
-    .use(initReactI18next)
-    .init({
-        fallbackLng: 'uz',
-        lang: i18n.language,
-        debug: true,
-        resources: {
-            uz: { translation: uzTranslation },
-            ru: { translation: ruTranslation },
-            en: { translation: enTranslation }
-        }
-    })
-
+  .use(LanguageDetector) // Tilni avtomatik aniqlash
+  .use(initReactI18next) // React bilan i18n ni bog'lash
+  .init({
+    fallbackLng: 'uz', // Default til
+    lng: i18n.language, // Joriy til
+    debug: true, // Debug rejim
+    resources: {
+      uz: { translation: uzTranslation },
+      ru: { translation: ruTranslation },
+      en: { translation: enTranslation },
+    },
+  });
 
 export default i18n;
