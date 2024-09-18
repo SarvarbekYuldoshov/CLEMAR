205ba8b4b8356caae3450b1a9ccc7cd80ae23b2e
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-18 17:52:51 +0300

    salom

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Dropdown } from 'antd';
+import 'antd/dist/reset.css';
+import Rasm_1 from "../images/rasm1.png"
 
 const Header = () => {
+  const { t } = useTranslation();
+
+  const items = [
+    {
+      key: '1',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="#home">
+          {t("header.header_text")}
+        </a>
+      ),
+    },
+    {
+      key: '2',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="#home1">
+          {t("header.header_text_1")}
+        </a>
+      ),
+    },
+    {
+      key: '3',
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="#home2">
+          {t("header.header_text_2")}
+        </a>
+      ),
+    },
+  ];
+
   return (
-    <div className=''>
-      <div className=''>
-        
+    <div className="bg-white shadow-md p-4 mt-[100px]">
+      <div className="max-w-[1200px] mx-auto flex justify-between items-center">
+        <Dropdown
+          className="w-[50px]"
+          menu={{ items }}
+          placement="bottomRight"
+        >
+          <button className="flex items-center p-2 bg-transparent text-white rounded hover:bg-[white] transition-colors">
+            <img
+              className="w-10 h-10"
+              src={Rasm_1} 
+              alt="Menu"
+            />
+          </button>
+        </Dropdown>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,24 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import "./Layout.css"
-import { Button, Form, Input, Modal } from 'antd';
+import { Button, Form, Input, Modal, message } from 'antd';
+
 const Layout = () => {
   const { t, i18n } = useTranslation();
 
   const handleLanguageChange = (e) => {
     i18n.changeLanguage(e.target.value);
   };
+
   const [open, setOpen] = useState(false);
   const [form] = Form.useForm();
-  
+
+  useEffect(() => {
+    const savedName = localStorage.getItem('name');
+    const savedSurname = localStorage.getItem('surname');
+    const savedNumber = localStorage.getItem('number');
+    
+    form.setFieldsValue({
+      name: savedName || '',
+      surname: savedSurname || '',
+      number: savedNumber || '',
+    });
+  }, [form]);
+
   const sendMessage = () => {
     form.validateFields()
       .then((values) => {
         const { name, surname, number } = values;
+        localStorage.setItem('name', name);
+        localStorage.setItem('surname', surname);
+        localStorage.setItem('number', number);
+
         const token = "7288526920:AAH-vd_HYqMjr_qE5zG6idFBNxfFeMi9aFo";
         const chat_id = "6801549705";
         const url = `https://api.telegram.org/bot${token}/sendMessage`;
-        const messageText = `Ism: ${name}\nFamiliya: ${surname}\nNumber: ${number}\nMahsulot: ${selectedItem?.name}\nNarxi: ${selectedItem?.price}`;
+        const messageText = `Ism: ${name}\nFamiliya: ${surname}\nNumber: ${number}\nMahsulot: Product Name\nNarxi: 100`;
         fetch(url, {
           method: 'POST',
           headers: {
@@ -32,28 +50,29 @@ const Layout = () => {
           .then((res) => res.json())
           .then((res) => {
             if (res.ok) {
-              message.success("Ma'lumot yuborildi");
+              message.success(t("layout.layout_text_5"));
               form.resetFields();
               setOpen(false);
             } else {
-              message.error("Yuborishda xatolik yuz berdi");
+              message.error(t("layout.layout_text_6"));
             }
           })
           .catch((err) => {
             console.error(err);
-            message.error("Xatolik yuz berdi, iltimos keyinroq urinib ko'ring");
+            message.error(t("layout.layout_text_7"));
           });
       })
       .catch(() => {
-        message.error("Iltimos, barcha maydonlarni to'ldiring!");
+        message.error(t("layout.layout_text_8"));
       });
   };
-  
+
   const showModal = () => setOpen(true);
   const closeModal = () => {
     form.resetFields();
     setOpen(false);
   };
+
   return (
     <div className='bg-[#007aff] layout'>
       <div className='max-w-[1200px] mx-auto p-[20px] flex justify-between items-center'>
@@ -66,7 +85,7 @@ const Layout = () => {
             href="https://yandex.ru/navi/?ll=69.220719,41.290161&amp;panorama%5Bpoint%5D=69.220719,41.290161&amp;panorama%5Bdirection%5D=227.914276,-0.865454&amp;panorama%5Bspan%5D=90.000000,90.000000&amp;panorama%5Bid%5D=1486659497_804360267_23_1571374783"
             className='text-[#cbc9c9]'
           >
-            {t("layout.layout_text_1")}:
+            {t("layout.layout_text_1")}
           </a>
           <a
             href="https://yandex.ru/navi/?ll=69.220719,41.290161&amp;panorama%5Bpoint%5D=69.220719,41.290161&amp;panorama%5Bdirection%5D=227.914276,-0.865454&amp;panorama%5Bspan%5D=90.000000,90.000000&amp;panorama%5Bid%5D=1486659497_804360267_23_1571374783"
@@ -84,14 +103,14 @@ const Layout = () => {
               src="https://clemar.uz/static/media/instagram.15618e7f7e625b7694935dd597571837.svg"
               alt=""
             />
-            <p className="min-text text-[white]">{t("layout.layout_text_3")}</p>
+            <p className="min-text text-[white] my-[10px]">{t("layout.layout_text_3")}</p>
           </li>
           <li className='flex gap-[10px] items-center'>
             <img className='w-[30px] h-[20px]'
               src="https://clemar.uz/static/media/telegram.c595f804c186181bf4a1da46dcaaa231.svg"
               alt=""
             />
-            <p className='text-[white]'>{t("layout.layout_text_4")}</p>
+            <p className='text-[white] mt-[15px]'>{t("layout.layout_text_4")}</p>
           </li>
           <li className='text-[#ffff]'>
             <a
@@ -115,40 +134,40 @@ const Layout = () => {
             </select>
           </li>
           <li className=''>
-            <Button className='text-[blue] bg-[white] w-[140px] h-[40px] rounded-[5px] text-[15x] items-center hover:transition-transform duration-500 hover:scale-110' onClick={() => showModal({ name: 'Product Name', price: '100' })}>
+            <Button className='text-[blue] bg-[white] w-[140px] h-[40px] rounded-[5px] text-[15x] items-center hover:transition-transform duration-500 hover:scale-110' onClick={showModal}>
             {t("layout.layout_btn")}
-          </Button>
+            </Button>
 
-          <Modal open={open} footer={null} onCancel={closeModal}>
-            <h1 className='flex items-center justify-center text-[30px]'>{t("layout.layout_btn_1")}</h1>
-            <p className='justify-center text-[20px] text-[blue]'>{t("layout.layout_btn_2")}</p>
-            <Form form={form} layout="vertical">
-              <Form.Item
-                label="Ism"
-                name="name"
-                rules={[{ required: true, message: 'Ismingizni kiriting' }, { min: 5, message: 'Ism 5 tadan kam bo\'lmasligi kerak' }]}
-              >
-                <Input className='w-full h-[50px]' placeholder='Ismingizni kiriting' />
-              </Form.Item>
-              <Form.Item
-                label="Familiya"
-                name="surname"
-                rules={[{ required: true, message: 'Familiyangizni kiriting' }, { min: 5, message: 'Familiya 5 tadan kam bo\'lmasligi kerak' }]}
-              >
-                <Input className='w-full h-[50px]' placeholder='Familiyangizni kiriting' />
-              </Form.Item>
-              <Form.Item
-                label="Telefon raqam"
-                name="number"
-                rules={[{ required: true, message: 'Telefon raqamingizni kiriting' }, { pattern: /^\+998\d{9}$/, message: 'Telefon raqam +998 bilan boshlanib, 9 ta raqamdan iborat bo\'lishi kerak' }]}
-              >
-                <Input className='w-full h-[50px]' placeholder='+998' />
-              </Form.Item>
-              <Button className='w-full h-[50px]' onClick={sendMessage} type="primary">
-                Yuborish
-              </Button>
-            </Form>
-          </Modal>
+            <Modal open={open} footer={null} onCancel={closeModal}>
+              <h1 className='flex items-center justify-center text-[30px]'>{t("layout.layout_btn_1")}</h1>
+              <p className='justify-center text-[20px] text-[blue]'>{t("layout.layout_btn_2")}</p>
+              <Form form={form} layout="vertical">
+                <Form.Item
+                  label={t("layout.layout_btn_3")}
+                  name="name"
+                  rules={[{ required: true, message: t("layout.layout_text_11")}, { min: 5, message: t("layout.layout_text_8") }]}
+                >
+                  <Input className='w-full h-[50px]' placeholder={t("layout.layout_btn_6")} />
+                </Form.Item>
+                <Form.Item
+                  label={t("layout.layout_btn_4")}
+                  name="surname"
+                  rules={[{ required: true, message: t("layout.layout_text_12") }, { min: 5, message: t("layout.layout_text_9") }]}
+                >
+                  <Input className='w-full h-[50px]' placeholder={t("layout.layout_btn_7")} />
+                </Form.Item>
+                <Form.Item
+                  label={t("layout.layout_btn_5")}
+                  name="number"
+                  rules={[{ required: true, message: t("layout.layout_text_12") }, { pattern: /^\+998\d{9}$/, message: t("layout.layout_text_10") }]}
+                >
+                  <Input className='w-full h-[50px]' placeholder='+998' />
+                </Form.Item>
+                <Button className='w-full h-[50px]' onClick={sendMessage} type="primary">
+                  Yuborish
+                </Button>
+              </Form>
+            </Modal>
           </li>
         </ul>
       </div>
