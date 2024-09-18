3b2e5eb9ba47cdf61d1e534d35dc4a5fc3e3cc83
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-18 09:26:22 +0300

    salom

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 const Layout = () => {
   return (
     <div className=''>
-      
+      <div className='max-w-[1200px] mx-auto p-[20px] flex justify-between items-center'>
+         <ul className=''>
+            <img src="https://clemar.uz/static/media/location.8f50ce0bbe934fcda50814961114ab24.svg" alt="" />
+            <li className=''>
+                <a className='' href="">Location:</a>
+            </li>
+            <li className=''>
+                <a className='' href="">Toshkent</a>
+            </li>
+         </ul>
+      </div>
     </div>
   )
 }
