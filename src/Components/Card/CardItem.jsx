af6beca7188a239da5daecd535a28888c4a66a43
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-20 17:11:13 +0300

    salom'

--- src/Components/Card/CardItem.jsx
+++ src/Components/Card/CardItem.jsx
@@ -8,8 +8,8 @@ const CardItem = () => {
   return (
     <div className=''>
       <div className='max-w-[1200px] mx-auto p-[20px]'>
-          <ul className=''>
-              <h1 className=''>{t("menu.menu_text_2")}</h1>
+          <ul className='w-[220px] h-auto border border-[#cbcbce] shadow-[0_0_5px_0_rgba(0,0,0,0.2)] justify-center'>
+              <h1 className='mt-[10px] ml-[15px]'>{t("menu.menu_text_2")}</h1>
               <img className='w-[250px] h-[180px] mt-[50px] ' src={Rasm_1} alt="Rasm 12" />
               <p className='text-white'>{t("menu.menu_text_7")}</p>
           </ul>
