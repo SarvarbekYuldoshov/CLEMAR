fb68bf89f9f09db6b50f49f7c39d08fd742c2598
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-20 08:42:08 +0300

    salom

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,15 @@
+
+import Rasm_1 from "../images/rasm12.png"
+import Rasm_2 from "../images/rasm13.png"
+import Rasm_3 from "../images/rasm14.png"
+import Rasm_4 from "../images/rasm15.png"
+import Rasm_5 from "../images/rasm16.jpg"
+const Card = [
+    {
+         "id": 1,
+         text1:"Qog'oz mahsulotlari",
+         img:Rasm_1,
+         text2:"4 ta mahsulotlar"
+        
+    }
+]
