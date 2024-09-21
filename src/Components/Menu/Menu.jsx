c753dbf4e33beef086ccfdddb62948b037817fd7
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-21 12:14:15 +0300

    sagiuhrqughq

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-import CardItem from '../Card/CardItem';
 
 export default function App() {
+  const [photos, setPhotos] = useState([]);
+
+  const getUser = () => {
+    fetch('https://clean.maxone.uz/api/categories')
+      .then(res => res.json())
+      .then(data => {
+        setPhotos(data);
+        localStorage.setItem('photos', JSON.stringify(data));
+      });
+  };
+
+  useEffect(() => {
+    const storedPhotos = localStorage.getItem('photos');
+    if (storedPhotos) {
+      setPhotos(JSON.parse(storedPhotos));
+    } else {
+      getUser();
+    }
+  }, []);
 
   return (
+    <div className=''>
+      <h1 className=''></h1>
     <Swiper
       spaceBetween={30}
       centeredSlides={true}
@@ -22,11 +42,19 @@ export default function App() {
       modules={[Autoplay, Pagination, Navigation]}
       className="mySwiper"
     >
-      {[...Array(9).keys()].map((_, index) => (
-        <SwiperSlide>
-          
-        </SwiperSlide>
-      ))}
+      {
+        photos.slice(0, 10).map((item, index) => (
+          <SwiperSlide key={index}>
+              <div className='max-w-[1200px] mx-auto p-[20px]'>
+                  <ul className='card-list w-[200px] h-[270px] border border-[#cbcbce] shadow-[0_0_5px_0_rgba(0,0,0,0.2)] justify-center rounded-[10px]'>
+                      <h1 className='mt-[15px] ml-[15px] font-montserrat text-[1.2vw] no-underline'>{item.name_uz}</h1>
+                      <img className='w-[180px] h-[150px] mt-[50px] ml-[10px] ' src={item.image} alt="" />
+                  </ul>
+              </div>
+          </SwiperSlide>
+        ))
+      }
     </Swiper>
+    </div>
   );
 }
--- src/i18n.jsx
+++ src/i18n.jsx
@@ -1,17 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import uzTranslation from '../public/uzbek/uzbek.json'; // To'g'ri fayl
-import ruTranslation from '../public/russian/russian.json';
-import enTranslation from '../public/english/english.json';
+import uzTranslation from '../src/public/uzbek/uzbek.json';
+import ruTranslation from '../src/public/russian/russian.json';
+import enTranslation from '../src/public/english/english.json';
 
 i18n
-  .use(LanguageDetector) // Tilni avtomatik aniqlash
-  .use(initReactI18next) // React bilan i18n ni bog'lash
+  .use(LanguageDetector) 
+  .use(initReactI18next) 
   .init({
-    fallbackLng: 'uz', // Default til
-    lng: i18n.language, // Joriy til
-    debug: true, // Debug rejim
+    fallbackLng: 'uz',
+    lng: i18n.language, 
+    debug: true,
     resources: {
       uz: { translation: uzTranslation },
       ru: { translation: ruTranslation },
