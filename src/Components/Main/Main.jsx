0455bad5bee50c8d9bece18da180faba738a9769
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-23 23:41:53 +0300

    salom

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Main = () => {
   return (
     <div className=''>
-        <div className='max-w-[1200px]'>
+        <div className='max-w-[1200px] mx-auto'>
 
         </div>
     </div>
