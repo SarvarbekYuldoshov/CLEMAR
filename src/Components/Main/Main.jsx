6b3c3c19d35460789b539b6a16aea4c5633a6544
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-24 14:06:46 +0300

    salom

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-
+import Rasm_1 from "../images/rasm17.jpg"
+import Rasm_2 from "../images/rasm18.jpg"
+import Rasm_3 from "../images/rasm19.jpg"
+import Rasm_4 from "../images/rasm20.jpg"
 const Main = () => {
   const { t } = useTranslation();
   return (
@@ -8,8 +11,9 @@ const Main = () => {
       <div className='max-w-[1200px] mx-auto p-[20px] flex'>
          <ul className=''>
           <li className=''>
-            <img src="" alt="" />
+            <img src={Rasm_1} alt="" />
           </li>
+          
          </ul>
       </div>
     </div>
