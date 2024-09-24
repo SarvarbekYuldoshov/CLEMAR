dcf3b0a4a33500af9d2e2987f98a541258863870
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-24 15:01:04 +0300

    salom

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,17 +4,120 @@ import Rasm_1 from "../images/rasm17.jpg"
 import Rasm_2 from "../images/rasm18.jpg"
 import Rasm_3 from "../images/rasm19.jpg"
 import Rasm_4 from "../images/rasm20.jpg"
+import { Button, Form, Input, Modal } from 'antd';
 const Main = () => {
   const { t } = useTranslation();
+  const handleLanguageChange = (e) => {
+    i18n.changeLanguage(e.target.value);
+  };
+
+  const [open, setOpen] = useState(false);
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    const savedName = localStorage.getItem('name');
+    const savedSurname = localStorage.getItem('surname');
+    const savedNumber = localStorage.getItem('number');
+    
+    form.setFieldsValue({
+      name: savedName || '',
+      surname: savedSurname || '',
+      number: savedNumber || '',
+    });
+  }, [form]);
+
+  const sendMessage = () => {
+    form.validateFields()
+      .then((values) => {
+        const { name, surname, number } = values;
+        localStorage.setItem('name', name);
+        localStorage.setItem('surname', surname);
+        localStorage.setItem('number', number);
+
+        const token = "7288526920:AAH-vd_HYqMjr_qE5zG6idFBNxfFeMi9aFo";
+        const chat_id = "6801549705";
+        const url = `https://api.telegram.org/bot${token}/sendMessage`;
+        const messageText = `Ism: ${name}\nFamiliya: ${surname}\nNumber: ${number}\nMahsulot: Product Name\nNarxi: 100`;
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            chat_id,
+            text: messageText,
+          }),
+        })
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.ok) {
+              message.success(t("layout.layout_text_5"));
+              form.resetFields();
+              setOpen(false);
+            } else {
+              message.error(t("layout.layout_text_6"));
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            message.error(t("layout.layout_text_7"));
+          });
+      })
+      .catch(() => {
+        message.error(t("layout.layout_text_8"));
+      });
+  };
+
+  const showModal = () => setOpen(true);
+  const closeModal = () => {
+    form.resetFields();
+    setOpen(false);
+  };
   return (
     <div>
       <div className='max-w-[1200px] mx-auto p-[20px] flex'>
-         <ul className='w-[250px] h-[350px] border border-black rounded-[10px]'>
+         <ul className='w-[250px] h-[375px] border border-black rounded-[10px]'>
           <li className='border border-black w-[200px] h-[200px]  justify-center flex rounded-[10px] ml-[25px] mt-[20px]'>
             <img className='w-[150px] h-[150px] mt-[20px]' src={Rasm_1} alt="" />
           </li>
-          <p className='font-light text-lg italic'>{t("main.main_text_1")}</p>
-          <p className='font-light text-lg italic'>{t("main.main_text_5")}</p>
+          <p className='font-light text-lg text-[15px] ml-[10px] mt-[20px]'>{t("main.main_text_1")}</p>
+          <p className='font-bold text-black text-lg text-[15px] ml-[10px]'>{t("main.main_text_5")}</p>
+          <li className='flex justify-center'>
+          <Button className='text-[blue] bg-[#e0e4e9] w-[200px] h-[40px] rounded-[5px] text-[15x] items-center hover:transition-transform duration-500 hover:scale-110 max-md:hidden' onClick={showModal}>
+            {t("main.main_text_6")}
+            </Button>
+
+            <Modal open={open} footer={null} onCancel={closeModal}>
+              <h1 className='flex items-center justify-center text-[30px]'>{t("layout.layout_btn_1")}</h1>
+              <p className='justify-center text-[20px] text-[blue]'>{t("layout.layout_btn_2")}</p>
+              <Form form={form} layout="vertical">
+                <Form.Item
+                  label={t("layout.layout_btn_3")}
+                  name="name"
+                  rules={[{ required: true, message: t("layout.layout_text_11")}, { min: 5, message: t("layout.layout_text_8") }]}
+                >
+                  <Input className='w-full h-[50px]' placeholder={t("layout.layout_btn_6")} />
+                </Form.Item>
+                <Form.Item
+                  label={t("layout.layout_btn_4")}
+                  name="surname"
+                  rules={[{ required: true, message: t("layout.layout_text_12") }, { min: 5, message: t("layout.layout_text_9") }]}
+                >
+                  <Input className='w-full h-[50px]' placeholder={t("layout.layout_btn_7")} />
+                </Form.Item>
+                <Form.Item
+                  label={t("layout.layout_btn_5")}
+                  name="number"
+                  rules={[{ required: true, message: t("layout.layout_text_12") }, { pattern: /^\+998\d{9}$/, message: t("layout.layout_text_10") }]}
+                >
+                  <Input className='w-full h-[50px]' placeholder='+998' />
+                </Form.Item>
+                <Button className='w-full h-[50px]' onClick={sendMessage} type="primary">
+                  Yuborish
+                </Button>
+              </Form>
+            </Modal>
+            </li>
          </ul>
       </div>
     </div>
