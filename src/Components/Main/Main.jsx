9125c0dd7d9a45b1995adf8e52809f664bb450a3
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-24 14:04:46 +0300

    salom

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,33 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Main = () => {
-  const [photos, setPhotos] = useState([]);
   const { t } = useTranslation();
-
-  const getUser = () => {
-    fetch('https://clean.maxone.uz/api/top_products/')
-      .then(res => res.json())
-      .then(data => {
-        setPhotos(data);
-        localStorage.setItem('photos', JSON.stringify(data));
-      })
-      .catch(err => console.error("Failed to fetch data:", err));
-  };
-
-  useEffect(() => {
-    const storedPhotos = localStorage.getItem('photos');
-    if (storedPhotos) {
-      setPhotos(JSON.parse(storedPhotos));
-    } else {
-      getUser();
-    }
-  }, []);
-
   return (
     <div>
       <div className='max-w-[1200px] mx-auto p-[20px] flex'>
          <ul className=''>
-          
+          <li className=''>
+            <img src="" alt="" />
+          </li>
          </ul>
       </div>
     </div>
