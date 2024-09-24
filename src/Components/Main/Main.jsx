673402f141e4c712a725ff3204514a45a8651136
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-24 14:49:50 +0300

    salom

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -9,12 +9,12 @@ const Main = () => {
   return (
     <div>
       <div className='max-w-[1200px] mx-auto p-[20px] flex'>
-         <ul className=''>
-          <li className='border border-black w-[200px] h-[200px]  justify-center flex rounded-[10px]'>
+         <ul className='w-[250px] h-[350px] border border-black rounded-[10px]'>
+          <li className='border border-black w-[200px] h-[200px]  justify-center flex rounded-[10px] ml-[25px] mt-[20px]'>
             <img className='w-[150px] h-[150px] mt-[20px]' src={Rasm_1} alt="" />
           </li>
-          <p className=''>{t("main.main_text_1")}</p>
-          
+          <p className='font-light text-lg italic'>{t("main.main_text_1")}</p>
+          <p className='font-light text-lg italic'>{t("main.main_text_5")}</p>
          </ul>
       </div>
     </div>
