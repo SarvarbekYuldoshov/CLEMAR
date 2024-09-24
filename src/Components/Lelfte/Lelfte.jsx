cbe79d3bdfc40b8d1ed7c6a0b4a189bdddcd2c62
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-24 18:17:15 +0300

    salom

---
+++ src/Components/Lelfte/Lelfte.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Lelfte = () => {
+  return (
+    <div className=''>
+      <div className=''>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Lelfte
