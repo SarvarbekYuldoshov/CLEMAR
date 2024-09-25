42bbe02b045f85c514e2e4a62c5f75a8b22034f1
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-25 17:37:53 +0300

    salom

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+    const { t } = useTranslation();
   return (
     <div className=''>
       <div className=''>
-        
+        <div className=''>
+            <h1 className=''>{t("footer.footer_text_1")}</h1>
+            <p className=''></p>
+            <p class="min-text"></p>
+        </div>
       </div>
     </div>
   )
