2026074fa1f4740b05a238d41318445ac69037d3
Sarvarbek Yuldoshov <sarvarbekyuldoshov85@gmail.com>
2024-09-29 10:16:40 +0300

    Navbar qismini tugirlayapman

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -84,7 +84,7 @@ const Layout = () => {
           />
           <a
             href="https://yandex.ru/navi/?ll=69.220719,41.290161"
-            className="text-[#676565] max-sm:text-[10px]"
+            className="text-[#bcb8b8] max-sm:text-[10px]"
           >
             {t('layout.layout_text_1')}
           </a>
